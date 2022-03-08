6efbf8a3da35208ead727f5701606b5f3fc22c3e
Page Fault Coder <code@thepagefault.com>
2022-03-07 19:59:47 -0500

    add connection call

--- mqtt.go
+++ mqtt.go
@@ -1,13 +1,26 @@
 // Package mqtt is the main encompasing mqtt package
 package mqtt
 
+import (
+	"fmt"
+	"net"
+)
+
 // MQTT is the main struct for MQTT messaging
 type MQTT struct {
 	isConnected bool
+	connection  *net.Conn
 }
 
 // Connect will connect to the given server
 func (m *MQTT) Connect(url string, port uint16, useTLS bool) error {
+	targetURL := url + ":" + fmt.Sprintf("%d", port)
+	connection, err := net.Dial("tcp", targetURL)
+	if err != nil {
+		return err
+	}
+
+	m.connection = &connection
 	m.isConnected = true
 	return nil
 }
